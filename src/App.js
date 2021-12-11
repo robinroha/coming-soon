674c6955b4543d712fc09600c6f49e850af8fe6a
robin hardjowijono <robinhardjowijono@hotmail.com>
2021-12-11 22:11:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Robin Hardjo</h1>
+      <h3>Full-Stack Development</h3>
+    </div>
+  );
+}
+
+export default App;
